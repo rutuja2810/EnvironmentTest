1a91eb33ac5fc5ca271c8b26553b660fc985d984
rutuja2810 <92105785+rutuja2810@users.noreply.github.com>
2025-07-07 21:21:08 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ fi
 echo "WORKSPACE is: $WORKSPACE"
 export WSPACE_STAGE=${WORKSPACE}
 echo "WSPACE_STAGE is: $WSPACE_STAGE"
-
+#copy command
 #cp -rp $WSPACE_STAGE/ab/cde/* $WSPACE_STAGE/
 echo "Deployment Summary" > $WSPACE_STAGE/Depl_Summary.log
 echo "---------------------------------------" >> $WSPACE_STAGE/Summary.log
@@ -20,12 +20,15 @@ cat WSPACE_STAGE/Summary.log
 ls -lrt 
 
 echo "Starting while loop..."
-
+#while loop
 i=1
 while [ $i -le 3 ]; do
   echo "Iteration $i"
   ((i++))
   sleep 1
 done
+
+curl -s -o /dev/null -w "Status: %{http_code}\n" https://www.google.com
+
 echo "Deployment finished."
 
